191eec3817542d6f66fb2bab5b6957e95d4fc600
asrofurrizqi <asrofurrizqi@gmail.com>
2022-09-09 16:58:41 +0700

    first commit

---
+++ scripts/sortCarByYearDecendingly.js
@@ -0,0 +1,27 @@
+function sortCarByYearDescendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  for(let i = 1; i < result.length;i++){
+
+    // mengecek elemen di belakang nya
+    for(let j = i - 1; j > -1; j--){
+        
+        // membandingkan value , descending
+        if(result[j + 1].year > result[j].year){
+
+            // tukar posisi array
+            [result[j+1],result[j]] = [result[j],result[j + 1]];
+
+        }
+    }
+  };
+  // Rubah code ini dengan array hasil sorting secara descending
+  console.log(result)
+  return result;
+}
